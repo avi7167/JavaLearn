e8f7ec565bbd52b61cc3768482a2232192c9ddda
avi7167 <sawadeavi87@gmail.com>
2023-03-05 18:33:14 +0530

    added new program

---
+++ A_Introduction/A_Hello.java
@@ -0,0 +1,21 @@
+package A_Introduction;
+// The Above Given Line declares the package
+
+/* class name same as filename and access modifier public so that
+    it is accessible from anywhere */
+public class A_Hello{
+    public static void main(String[] args){
+
+        // System class, out is variable of type printstream and println is method
+        System.out.println("Hello, World!");
+
+        // different ways of printing in java
+
+        // this will not print a new line
+        System.out.print("OnlyPrint");
+
+        // we can use new line character 
+        System.out.print("Demo\n");
+        System.out.println("on newline");
+    }
+}
